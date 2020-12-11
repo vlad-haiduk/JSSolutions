cd9133b165fb32c933b2851afcb96c73d16906c3
LuxVenti <osiichuk100@gmail.com>
2020-12-11 19:05:58 +0200

    start

---
+++ funcOverload.js
@@ -0,0 +1,57 @@
+/**
+ * Перевантаження функцій
+ */
+class Overload {
+    #funcs = {};
+    /**
+     * @param  {...function} functions перелік функцій
+     */
+    constructor(...functions) {
+        // Формування об'єкта-визначника:
+        for (let func of functions) {
+            this.#funcs[func.length] = func;
+        }
+
+        // якщо не надано стандартної функції
+        if (!(0 in this.#funcs))
+            this.#funcs[0] = (...args) => undefined;
+    }
+
+    /**
+     * Виклик функції в залежності від кількості аргументів
+     * @param  {...any} args аргументи функції
+     * @returns функція
+     */
+    call(...args) {
+        return args.length in this.#funcs
+        ? this.#funcs[args.length](...args)
+        : this.#funcs[0](...args);
+    }
+}
+
+// Приклад
+class Summator {
+    #sumOverload = new Overload(this.#singleSum, this.#binarySum, this.#multipleSum);
+
+    #singleSum(a) {
+        console.log('single')
+        return a + a;
+    }
+
+    #binarySum(a, b) {
+        console.log('binary')
+        return a + b;
+    }
+
+    #multipleSum(...adds) {
+        console.log('multiply')
+        return adds.reduce((x, current) => x + current);
+    }
+
+    sum(...args) {
+        return this.#sumOverload.call(...args);
+    }
+}
+
+const action = new Summator();
+console.log(action.sum(1, 7));
