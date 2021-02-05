8c34c832f2bdba3602d762c934f4fb2dd744ba63
LuxVenti <48798654+LuxVenti@users.noreply.github.com>
2021-02-05 02:12:52 +0200

    Update Tuple.js

--- Tuple.js
+++ Tuple.js
@@ -1,20 +1,41 @@
-/** Finite ordered list of elements */
+/** Finite ordered constant list of elements */
 class Tuple {
-    /**
-     * @param  {...any} values list of tuple values
-     */
+    #len = 0;
+
+    /** @param  {...any} values list of tuple values */
     constructor(...values) {
-        Object.defineProperty(this, 'length', {
-            value: 0,
+        Object.defineProperty(this, 'len', {
             enumerable: false,
             writable: true
         })
 
         for (let item of values) {
-            Object.defineProperty(this, this.length++, {
+            Object.defineProperty(this, this.#len++, {
                 value: item,
-                writable: false
+                writable: false,
+                enumerable: true
             })
         }
+    }
+
+    /** Amount of elements */
+    get length() {
+        return this.#len;
+    }
+
+    *[Symbol.iterator]() {
+        let current = 0;
+        while (current < this.#len) {
+            yield this[current++];
+        }
+    }
+
+    /** String representing the collection */
+    toString() {
+        let primitive = '(';
+        for (let item of this) {
+            primitive += item + ', ';
+        }
+        return primitive.slice(0, -2) + ')';
     }
 }
